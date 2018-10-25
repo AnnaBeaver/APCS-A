a473b70672f1f248076c25bd6b9049bee5a5ddc6
Anna X. Beaver <axbeaver@WS-ACADBUS.EDU>
2018-10-25 10:31:30 -0700

    not near working yet
    instructions in email

---
+++ StudentList/StudListRunner.java
@@ -0,0 +1,13 @@
+
+
+public class StudListRunner
+{
+    while(no one's typed anything){
+        studList.addStudentToList();
+        studList.deleteStudentFromList();
+        studList.editStudentList();
+        studList.clearList();
+        studList.printAll();
+        studList.printStudent();
+    }
+}
