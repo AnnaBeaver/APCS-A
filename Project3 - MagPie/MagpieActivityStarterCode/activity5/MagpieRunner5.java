035224d761aad343cfa3d5abdd9b00cda64926f2
Anna <793308@seq.org>
2018-11-13 09:21:04 -0800

    transferred from another repository

---
+++ Project3 - MagPie/MagpieActivityStarterCode/activity5/MagpieRunner5.java
@@ -0,0 +1,61 @@
+<<<<<<< HEAD
+import java.util.Scanner;
+
+/**
+ * A simple class to run the Magpie class.
+ * @author Laurie White
+ * @version April 2012
+ */
+public class MagpieRunner5
+{
+
+	/**
+	 * Create a Magpie, give it user input, and print its replies.
+	 */
+	public static void main(String[] args)
+	{
+		Magpie5 maggie = new Magpie5();
+		
+		System.out.println (maggie.getGreeting());
+		Scanner in = new Scanner (System.in);
+		String statement = in.nextLine();
+		
+		while (!statement.equals("Bye"))
+		{
+			System.out.println (maggie.getResponse(statement));
+			statement = in.nextLine();
+		}
+	}
+
+}
+=======
+import java.util.Scanner;
+
+/**
+ * A simple class to run the Magpie class.
+ * @author Laurie White
+ * @version April 2012
+ */
+public class MagpieRunner5
+{
+
+	/**
+	 * Create a Magpie, give it user input, and print its replies.
+	 */
+	public static void main(String[] args)
+	{
+		Magpie5 maggie = new Magpie5();
+		
+		System.out.println (maggie.getGreeting());
+		Scanner in = new Scanner (System.in);
+		String statement = in.nextLine();
+		
+		while (!statement.equals("Bye"))
+		{
+			System.out.println (maggie.getResponse(statement));
+			statement = in.nextLine();
+		}
+	}
+
+}
+>>>>>>> 75a76530cea06f81e6fe56e9dc77afb39db7d760
