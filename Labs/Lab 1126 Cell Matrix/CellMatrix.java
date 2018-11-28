00fbf66cd32df1a134918ef1b1cf53bc46285b4c
Anna <793308@seq.org>
2018-11-28 09:33:06 -0800

    Taken out extra returns

--- Labs/Lab 1126 Cell Matrix/CellMatrix.java
+++ Labs/Lab 1126 Cell Matrix/CellMatrix.java
@@ -1,20 +1,12 @@
 
 public class CellMatrix{
-    /**
-     * Instance variables
-     */
+    //Instance Variables
     private Cell[][] cells;
-
-    /**
-     * Constructor
-     */
+    //Constructor
     public CellMatrix(int a, int b){
         cells = new Cell[a][b];
     }
-
-    /**
-     * Methods
-     */
+    //Methods
     //Print each row and column of nums to the console
     public void printMatrix(){
         for(int a = 0; a<cells.length;a++){
@@ -24,7 +16,6 @@ public class CellMatrix{
             System.out.println( );
         }
     }
-
     //  load nums with random numbers between 1 and 10 inclusive
     public void loadMatrix(){
         for(int a = 0; a<cells.length;a++){
@@ -33,14 +24,13 @@ public class CellMatrix{
             }
         }
     }
-    
     //loads the sum array so that each cell has the sum of it's neighbors in it
     public void loadSum(CellMatrix cm2){
         for(int a = 0; a < cells.length;a++){
             for(int b = 0; b < cells[0].length;b++){
                 int var = 0;
                 if(b > 0){
-                   var = var + cm2.cells[a][b-1].getInt();
+                    var = var + cm2.cells[a][b-1].getInt();
                 }
                 if ((b+1) < cells.length){
                     var = var + cm2.cells[a][b+1].getInt();
@@ -51,11 +41,10 @@ public class CellMatrix{
                 if ((a+1) < cells.length){
                     var = var + cm2.cells[a+1][b].getInt();
                 }
-               cells[a][b] = new Cell(var);
+                cells[a][b] = new Cell(var);
             }
         }
     }
-    
     //uses the sum array to check every spot and save the largest one
     //as GN, which it then returns
     public int getGreatestNeighbors(){
